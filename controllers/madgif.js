5df8ad8f7c84adfb8e9b08dbd722c5c68f968870
krichdev <kgrichardson2@gmail.com>
2017-03-08 15:05:19 -0800

    displaying gifs on story page, and wrote mad lib

--- controllers/madgif.js
+++ controllers/madgif.js
@@ -57,24 +57,9 @@ router.get('/story', function(req, res){
   }
 
   async.series([tvShow, celeb, reaction, emotion], function(err, results) {
-    console.log(results);
+    res.render('story', {results: results});
   });
 
 })
-
-
-// var q = req.query.gif;
-// giphy.search({
-//     q: q,
-//     rating: 'pg-13',
-//     limit: 1
-// }, function (err, response) {
-//     // Res contains gif data!
-//     var gifURL = response.data[0].images.downsized.url;
-//     res.render('story', {result: gifURL})
-//     // console.log(response.data[0].images.downsized.url)
-// });
-
-
 
 module.exports = router;
