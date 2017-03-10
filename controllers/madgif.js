bb2dcb4651fcd2e1fde04a9a641b57ac569b6950
krichdev <kgrichardson2@gmail.com>
2017-03-10 10:22:41 -0800

    synced databases together, and getting data for favorite post

--- controllers/madgif.js
+++ controllers/madgif.js
@@ -14,8 +14,7 @@ router.get('/story', function(req, res){
     var q = req.query.tvshow;
     giphy.search({
       q: q,
-      rating: 'pg-13',
-      // limit: 1
+      rating: 'pg-13'
     }, function(err, response) {
       var tvGif = response.data[r].images.downsized.url;
       callback(null, tvGif);
@@ -26,8 +25,7 @@ router.get('/story', function(req, res){
     var q = req.query.celeb;
     giphy.search({
       q: q,
-      rating: 'pg-13',
-      // limit: 1
+      rating: 'pg-13'
     }, function(err, response) {
       var celebGif = response.data[r].images.downsized.url;
       callback(null, celebGif);
@@ -38,8 +36,7 @@ router.get('/story', function(req, res){
     var q = req.query.reaction;
     giphy.search({
       q: q,
-      rating: 'pg-13',
-      // limit: 1
+      rating: 'pg-13'
     }, function(err, response) {
       var reactionGif = response.data[r].images.downsized.url;
       callback(null, reactionGif);
@@ -50,8 +47,7 @@ router.get('/story', function(req, res){
     var q = req.query.emotion;
     giphy.search({
       q: q,
-      rating: 'pg-13',
-      // limit: 1
+      rating: 'pg-13'
     }, function(err, response) {
       var emotionGif = response.data[r].images.downsized.url;
       callback(null, emotionGif);
--- controllers/profile.js
+++ controllers/profile.js
@@ -2,11 +2,20 @@ var express = require('express');
 var giphy = require('giphy-api')();
 var isLoggedIn = require('../middleware/isLoggedIn')
 var flash = require('connect-flash');
+var db = require('../models');
 var router = express.Router();
 
-
+//Render Profile Page
 router.get('/', function(req, res){
   res.render('profile');
+});
+
+//POST favorite mad gif to user profile
+router.post('/', function(req, res){
+  console.log(req.body);
+  db.favorite.create(req.body).then(function(){
+    res.redirect('/profile')
+  })
 });
 
 
